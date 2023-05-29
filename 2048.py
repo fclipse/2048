d5a834123dc20a5a44eb858ccade374ba37f00bf
한성준 <june123000@naver.com>
2023-05-29 23:27:59 +0900

    Update 2048.py
    [ADD] / 230529 / added screen clear feature, removed test printing

--- 2048.py
+++ 2048.py
@@ -8,25 +8,17 @@
 # 5. 처음에 2가 2개 
 
 # 선언부
-import copy, random
+import copy, random, os
 
 zeroLocation = []
 board = []                                  # 만들 때 항상 형태 주의. 이차원 리스트인지 일차원 리스트인지 편한 걸로 생각할 수도 있음.
 num = [2, 2, 2, 2, 2, 2, 2, 2, 2, 4]        # 그냥 if randint(0, 9) == 0 :일때만 4넣을 수도 있음
 score = 0
+size = 0
 key = {'w' : 0, 'a' : 1, 's' : 2, 'd' : 3}
 move = [[0, -1], [-1, 0], [0, 1], [1, 0]]   # **y좌표는 내려갈수록 증가함
 #print('board status :', board )
 
-print('Welcome to 2048 game')
-size = int(input('board size input (more than 3) :'))
-board = [[0 for j in range(size)] for i in range(size)]
-#print('board init :')
-#print(board)
-#for i in range(size):
-#    for j in range(size):
-#        print(board[i][j], end = ' ')
-#    print()
 
 def isEnd():
     global board
@@ -104,82 +96,97 @@ def maxNum():
     return num
 
 
-# 입력부
-#q1 = input('Start game? y/n')
-round = 0
+if __name__ == '__main__':
+	print('Welcome to 2048 game')
+	env = int(input('Enter your environment (Window - 1, Linux - 2) : '))
+	size = int(input('board size input (more than 3) :'))
+	board = [[0 for j in range(size)] for i in range(size)]
+	#print('board init :')
+	#print(board)
+	#for i in range(size):
+	#    for j in range(size):
+	#        print(board[i][j], end = ' ')
+	#    print()
+	
+	round = 0
+	print('Game start')
+	round = 1
+	# 실행부
+	# 초기 랜덤 숫자 2개
 
-#if q1 == 'n':
-#    print('See you next time')
-#    round = -1    
-#else:
-print('Game start')
-round = 1
-# 실행부
-# 초기 랜덤 숫자 2개
-
-for i in range(2):
-    coordinate = [random.randint(0, size-1), random.randint(0, size-1)]
-    
-    while board[coordinate[1]][coordinate[0]] != 0:             # 빈칸에 생성
-        coordinate = [random.randint(0, size-1), random.randint(0, size-1)]
-    board[coordinate[0]][coordinate[1]] = 2
-
-# 계속 반복
-while round > 0:
-    # 출력
-    print()
-    maxNumber = maxNum()
-    print('round : %4d / score : %d / max Number : %d' % (round, score, maxNumber))
-    printBoard()
-    # 입력받음
-    direction = input('press w/a/s/d :')
-    if direction not in key:
-        print('wrong key input')
-        continue
-    else:       # 움직일 때
-        # 첫 번째 돌림
-        for i in range(key[direction]):
-            rotate()
-        
-        # 숫자 움직이기
-        moved = 0       # 움직이지 않으면 안됨.
-        for i in range(size):
-            for j in range(size):
-                merged = 0                  # 턴마다 한 번씩만 merge해줘야 함.
-                if board[i][j] != 0:
-                    for k in range(i):      # y좌표 내려온 횟수만큼 반복
-                        if board[i - k - 1][j] == 0:                    # 위에 비어있으면 올려보냄
-                            swap(i - k - 1, j, i - k, j)
-                            moved += 1
-                        elif board[i - k - 1][j] == board[i - k][j] and merged == 0:        # 같을 때 합침
-                            merge(i - k - 1, j, i - k, j)                   # 올려보내기를 먼저 해서 바로 위에 숫자가 있을 경우에만 합침. 중간에 0이 있을 경우 없음.
-                            merged += 1
-                            moved += 1
-        
-        # 두 번째 돌림
-        for i in range(4 - key[direction]):
-            rotate()
-            
-        if moved == 0:
-                continue        # 바뀐 게 없으면 다시 한번.
-    
-    # 오류 확인용
-    #print('before create random number')
-    #printBoard()
-    # 끝났는지 판단
-    if isEnd() :
-        break
-    else:
-        round += 1
-        # 랜덤한 위치에 랜덤한 숫자 생성(4와 2비율은 1:9정도)
-        createRandomNumber()
-        print(zeroLocation)
-
-#일단은 2048찍고 게임 끝날 경우에도 승리로 판정
-maxNumber = maxNum()
-if maxNumber >= 2048:
-    print('You Win!')
-    print('Congratuations! your max Number is %d and Score is %d' % (maxNumber, score))
-else:
-    print('You Lose')
-    print('Wawawa... See you next time! Your max Number is %d and score is %d' % (maxNumber, score))
+	for i in range(2):
+	    coordinate = [random.randint(0, size-1), random.randint(0, size-1)]
+	    
+	    while board[coordinate[1]][coordinate[0]] != 0:             # 빈칸에 생성
+	        coordinate = [random.randint(0, size-1), random.randint(0, size-1)]
+	    board[coordinate[0]][coordinate[1]] = 2
+	
+	# moved 설정
+	moved = 1
+	# 계속 반복
+	while round > 0:
+	    # 출력
+	    print()
+	    if env == 1:
+		    os.system('clear')	# 화면 지우는 부분 - Window
+	    else:
+		    os.system('clear')	# 화면 지우는 부분 - Linux
+	    if moved == 0:
+		    print('You should move any block at least once!')
+	    maxNumber = maxNum()
+	    print('round : %4d / score : %d / max Number : %d' % (round, score, maxNumber))
+	    print('-------------------')
+	    printBoard()
+	    # 입력받음
+	    print('-------------------')
+	    direction = input('press w/a/s/d : ')
+	    if direction not in key:
+	        print('wrong key input')
+	        continue
+	    else:       # 움직일 때
+	        # 첫 번째 돌림
+	        for i in range(key[direction]):
+	            rotate()
+	        
+	        # 숫자 움직이기
+	        moved = 0       # 움직이지 않으면 안됨.
+	        for i in range(size):
+	            for j in range(size):
+	                merged = 0                  # 턴마다 한 번씩만 merge해줘야 함.
+	                if board[i][j] != 0:
+	                    for k in range(i):      # y좌표 내려온 횟수만큼 반복
+	                        if board[i - k - 1][j] == 0:                    # 위에 비어있으면 올려보냄
+	                            swap(i - k - 1, j, i - k, j)
+	                            moved += 1
+	                        elif board[i - k - 1][j] == board[i - k][j] and merged == 0:        # 같을 때 합침
+	                            merge(i - k - 1, j, i - k, j)                   # 올려보내기를 먼저 해서 바로 위에 숫자가 있을 경우에만 합침. 중간에 0이 있을 경우 없음.
+	                            merged += 1
+	                            moved += 1
+	        
+	        # 두 번째 돌림
+	        for i in range(4 - key[direction]):
+	            rotate()
+	            
+	        if moved == 0:
+	            continue        # 바뀐 게 없으면 다시 한번.
+	    
+	    # 오류 확인용
+	    #print('before create random number')
+	    #printBoard()
+	    # 끝났는지 판단
+	    if isEnd() :
+	        break
+	    else:
+	        round += 1
+	        # 랜덤한 위치에 랜덤한 숫자 생성(4와 2비율은 1:9정도)
+	        createRandomNumber()
+	        # print(zeroLocation)
+	
+	#일단은 2048찍고 게임 끝날 경우에도 승리로 판정
+	maxNumber = maxNum()
+	if maxNumber >= 2048:
+	    print('You Win!')
+	    print('Congratuations! your max Number is %d and Score is %d' % (maxNumber, score))
+	else:
+	    print('You Lose')
+	    print('Wawawa... See you next time! Your max Number is %d and score is %d' % (maxNumber, score))
